8d87790a83c2c29d8d501074067e18a7cfb5095c
Anaum Riaz <anaum.riaz91@gmail.com>
2017-05-26 17:50:36 -0700

    Update dictionary.h

--- CS50/Pset5/Linked List/dictionary.h
+++ CS50/Pset5/Linked List/dictionary.h
@@ -11,33 +11,10 @@
 #define DICTIONARY_H
 #include <stdbool.h>
 
-//size of hashtable
-#define HTSIZE 6200
-
 
 // maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
-
-//globalizing the number of words read from dictionary
-int n_words_dic;
-
-//our struct for linked list
-typedef struct node
-{
-    char wordic[LENGTH + 1];
-    struct node *next;
-} node ;
-
-//function declarations for operation
-//takes a string and gives a hash code
-int hashfn(const char *str);
-
-//returns the size of a particular bucket
-int size_ll(node*);
-
-//inserts a value in the hashtable
-void insert(const char*);
 
 //displays the hashtable and size of individual linked lists
 void display();
@@ -66,10 +43,5 @@ bool unload(void);
 //inserts element in Linkedlist
 void insert(const char*);
 
-//displays the elements of a linked list 
-void display();
-
-//deletes a linked list
-void delete_ll(node* );
 
 #endif // DICTIONARY_H
